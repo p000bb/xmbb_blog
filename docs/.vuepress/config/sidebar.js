a28b6e85c236a72a0961d4eddb5ef7776f550acd
p000bb <p000bb@qq.com>
2020-11-01 17:29:23 +0800

    自动化部署测试

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -1,15 +1,4 @@
 // 此文件没有用到，仅用于测试和侧边栏数据格式的参考。
 
 module.exports = { // 侧边栏
-	'/web/': [{
-		title: 'HTML5',
-		collapsable: true,
-		children: [
-			'html5/HTML5的语义元素',
-			'html5/HTML5多媒体标签',
-			'html5/HTML5表单元素',
-			'html5/HTML5中的API',
-			'html5/Canvas'
-		]
-	}, ]
 }
