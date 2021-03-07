7ebe6b88a97a8d02a0312aaf195296a33b56986f
p000bb <934371212@qq.com>
2021-03-07 16:29:09 +0800

    电影时间判断手机pc端

--- docs/.vuepress/mixins/index.js
+++ docs/.vuepress/mixins/index.js
@@ -1,20 +1,25 @@
 export default {
-  methods:{
-    isPhone(){
-      var userAgentInfo = navigator.userAgent;
- 
-    var mobileAgents = [ "Android", "iPhone", "SymbianOS", "Windows Phone", "iPad","iPod"];
- 
-    var mobile_flag = false;
- 
-    //根据userAgent判断是否是手机
-    for (var v = 0; v < mobileAgents.length; v++) {
-        if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
-            mobile_flag = true;
-            break;
-        }
+  methods: {
+    isPhone() {
+      //   var userAgentInfo = navigator.userAgent;
+
+      // var mobileAgents = [ "Android", "iPhone", "SymbianOS", "Windows Phone", "iPad","iPod"];
+
+      var mobile_flag = false;
+
+      // //根据userAgent判断是否是手机
+      // for (var v = 0; v < mobileAgents.length; v++) {
+      //     if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
+      //         mobile_flag = true;
+      //         break;
+      //     }
+      //   }
+      //   console.log(mobile_flag)
+      //   return mobile_flag;
+      // }
+      if (window.screen.width <= 768) {
+        mobile_flag = true;
       }
-      console.log(mobile_flag)
       return mobile_flag;
     }
   }
